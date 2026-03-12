fdd025086a8915984b4bb6438dfcbbbd5aaaefc0
NataliaFiltanovych <natashe4ka21092016@gmail.com>
2026-03-12 13:55:43 +0000

    Deploying to gh-pages from @ NataliaFiltanovych/goit-js-hw-10@0f582e3d390cee8f74e38cad9bbdeab50b3ea507 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("Snackbar");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as s}from"./assets/vendor-BbbuE1sJ.js";const m=document.querySelector("form");m.addEventListener("submit",t=>{t.preventDefault();const o=t.target.elements.delay.value,i=t.target.elements.state.value;new Promise((e,r)=>{setTimeout(()=>{i==="fulfilled"?e(o):r(o)},o)}).then(e=>{s.show({message:`✅ Fulfilled promise in ${e}ms`,messageColor:"white",color:"green",position:"topRight"})}).catch(e=>{s.show({message:`❌ Rejected promise in ${e}ms`,messageColor:"white",color:"red",position:"topRight"})})});
 //# sourceMappingURL=2-snackbar.js.map
